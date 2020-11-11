bd702ee0d6e7b1aadc427adbeba3960f6b1d3de5
Igor Veyner <igorveyner95@gmail.com>
2020-11-11 17:36:05 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def redirect_to_signup_if_not_logged_in
+    redirect_to root_path unless session[:user_id]
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+  end
+
+  def admin
+    current_user.admin if session[:user_id]
+  end
 end
